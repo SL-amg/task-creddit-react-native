c491fbd175efad0f62f5ace49629237e48d335ac
Abdullah Abbas <sl_amg@msn.com>
2024-12-26 20:36:15 +0300

    added a new post

--- creddit/components/PostAdd.js
+++ creddit/components/PostAdd.js
@@ -1,21 +1,56 @@
 import {
-    Button,
-    StyleSheet,
-    Text,
-    View,
-    TextInput,
-    SearchBar,
-    Image,
-    TouchableOpacity,
-  } from "react-native";
-
+  Button,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  SearchBar,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import React, { useState } from "react";
+import { addNewPost } from "../api/post";
+import { useNavigation } from "@react-navigation/native";
+import { useMutation } from "@tanstack/react-query";
 
 const PostAdd = () => {
-  return (
- <View>
-        <Text>This is the add post page</Text>
-        </View>
-  )
-}
+  const navigation = useNavigation();
 
-export default PostAdd
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const createMutation = useMutation({
+    mutationKey: ["createPost"],
+    mutationFn: (newPost) => addNewPost(newPost),
+    onSucess: () => {
+      navigation.navigate("Post List");
+    },
+  });
+  const handleSubmit=()=>{
+    console.log(title)
+    createMutation.mutate({
+      title ,
+      description ,
+    });}
+
+  return (
+    <View>
+      <TextInput
+        placeholder="Title"
+        onChangeText={(e) => {
+            setTitle(e);
+        }}
+      />
+      <TextInput
+        placeholder="Description"
+        onChangeText={(e) => {
+            setDescription(e);
+        }}
+      />
+      <Button title="submit" onPress={handleSubmit}/>
+
+    </View>
+  );
+};
+
+export default PostAdd;
